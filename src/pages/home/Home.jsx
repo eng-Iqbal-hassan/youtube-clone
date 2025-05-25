6f764f4133269103e3ba1bbb88046d9a7da2942e
Iqbal Hassan <iqbal.hassan@cogentlabs.co>
2025-05-25 22:53:24 +0500

    home page is completed

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,16 @@
+import Feed from "../../components/feed/Feed.jsx";
+import Sidebar from "../../components/sidebar/Sidebar.jsx";
 import "./Home.css";
-const Home = () => {
-  return <div>This is home page</div>;
+
+const Home = ({ sidebar }) => {
+  return (
+    <>
+      <Sidebar sidebar={sidebar} />
+      <div className={`container ${sidebar ? "" : "large-container"}`}>
+        <Feed />
+      </div>
+    </>
+  );
 };
 
 export default Home;
