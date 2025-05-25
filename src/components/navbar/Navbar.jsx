99b5fe634d549e8c548223e8cc2ae3cd353fe3a2
Iqbal Hassan <iqbal.hassan@cogentlabs.co>
2025-05-25 14:39:30 +0500

    header UI is completed

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,33 @@
+import "./Navbar.css";
+import menu_icon from "../../assets/menu.png";
+import logo from "../../assets/logo.png";
+import search_icon from "../../assets/search.png";
+import upload_icon from "../../assets/upload.png";
+import more_icon from "../../assets/more.png";
+import notification_icon from "../../assets/notification.png";
+import profile_icon from "../../assets/jack.png";
+
+const Navbar = () => {
+  return (
+    <nav className='flex-div'>
+      <div className='flex-div nav-left'>
+        <img className='menu-icon' src={menu_icon} alt='menu' />
+        <img className='logo' src={logo} alt='logo' />
+      </div>
+      <div className='flex-div nav-middle'>
+        <div className='search-box flex-div'>
+          <input type='text' placeholder='search' />
+          <img src={search_icon} alt='search' />
+        </div>
+      </div>
+      <div className='flex-div nav-right'>
+        <img src={upload_icon} alt='upload' />
+        <img src={more_icon} alt='more' />
+        <img src={notification_icon} alt='notification' />
+        <img src={profile_icon} className='user-icon' alt='profile' />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
